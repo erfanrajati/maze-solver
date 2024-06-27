e7c40d7e5c35b055387d2f3da7be41844925face
Erfan Rajati Haghi <erfan.rajati@gmail.com>
2024-06-28 01:30:10 +0330

    Files Updated

--- MazeGraph.java
+++ MazeGraph.java
@@ -2,14 +2,37 @@ import java.util.*;
 
 public class MazeGraph 
 {
-    ArrayList<int[]> edges = new ArrayList<>();
-    ArrayList<Integer> vertices = new ArrayList<>();
+    private class Vertex
+    {
+        int i, j = 0;
+        public Vertex(int i, int j)
+        {
+            this.i = i;
+            this.j = j;
+        }
+    }
+
+    private class Edge 
+    {
+        Vertex[] edge;
+    
+        public Edge(Vertex v, Vertex u) 
+        {
+            this.edge[0] = v;
+            this.edge[1] = u;
+        }
+    }
+    
+
+    ArrayList<Vertex> vertices = new ArrayList<>();
+    ArrayList<Edge> edges = new ArrayList<>();
 
     int[][] mazeMatrix = {{}};
 
     public MazeGraph(int height, int width) 
     {
-        try (Scanner inputMatrix = new Scanner(System.in)) {
+        try (Scanner inputMatrix = new Scanner(System.in)) 
+        {
             for (int i = 0; i < height; i++) 
             {
                 for (int j = 0; j < width; j++)
@@ -20,14 +43,14 @@ public class MazeGraph
         }
     }
 
-    public void addVertex(int v) 
-    {
-        vertices.add(v);
-    }
+    // public void addVertex(int v) 
+    // {
+    //     vertices.add(v);
+    // }
 
-    public void addEdge(int v, int u) 
-    {
-        int[] edge = {v, u};
-        edges.add(edge);
-    }
+    // public void addEdge(int v, int u) 
+    // {
+    //     int[] edge = {v, u};
+    //     edges.add(edge);
+    // }
 }

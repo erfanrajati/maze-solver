568f18fc5b7f3aa7effb1096c623087808cb656b
Erfan Rajati Haghi <erfan.rajati@gmail.com>
2024-06-28 04:19:25 +0330

    Part 1 of maze traversal algorithm implemented.

--- Main.java
+++ Main.java
@@ -2,19 +2,10 @@ import java.util.Scanner;
 
 public class Main 
 {
-    int[][][] vertexGuide = {
-        {
-            {1, 1, 1},
-            {1, 0, 0},
-            {1, 0, 1}
-        }
-    };
-
     Scanner userIn = new Scanner(System.in);
     int mazeHeight = userIn.nextInt();
     int mazeWidth = userIn.nextInt();
     MazeGraph maze = new MazeGraph(mazeHeight, mazeWidth);
-
 
     public void main(String[] args) 
     {
--- MazeGraph.java
+++ MazeGraph.java
@@ -1,10 +1,20 @@
 import java.util.*;
 
+enum Direction
+{
+    LEFT,
+    DOWN,
+    RIGHT,
+    UP
+}
+
+
 public class MazeGraph 
 {
     private class Vertex
     {
-        int i, j = 0;
+        int i = 0;
+        int j = 0;
         public Vertex(int i, int j)
         {
             this.i = i;
@@ -23,23 +33,108 @@ public class MazeGraph
         }
     }
     
+    private class Current
+    {
+        int[] pose = {1, 0};
+        Direction face;
+        int[] right;
+
+        public Current()
+        {
+            this.face = Direction.RIGHT;
+            this.right[0] = 1;
+            this.right[1] = 0;
+        }
+
+        public void advance() 
+        {
+            if (this.face == Direction.RIGHT) 
+                this.pose[0] += 1;
+            if (this.face == Direction.DOWN) 
+                this.pose[1] += 1;
+            if (this.face == Direction.LEFT) 
+                this.pose[0] -= 1;
+            if (this.face == Direction.UP) 
+                this.pose[1] -= 1;
+        }
+
+        public void turnRight()
+        {
+            if (this.face == Direction.RIGHT) 
+            {
+                this.face = Direction.DOWN;
+                this.right[0] = 0;
+                this.right[1] = -1;
+            }
+            if (this.face == Direction.DOWN) 
+            {
+                this.face = Direction.LEFT;
+                this.right[0] = -1;
+                this.right[1] = 0;
+            }
+            if (this.face == Direction.LEFT) 
+            {
+                this.face = Direction.UP;
+                this.right[0] = 0;
+                this.right[1] = 1;
+            }
+            if (this.face == Direction.UP) 
+            {
+                this.face = Direction.RIGHT;
+                this.right[0] = 1;
+                this.right[1] = 0;
+            }
+        }
+    }
 
     ArrayList<Vertex> vertices = new ArrayList<>();
     ArrayList<Edge> edges = new ArrayList<>();
 
+    int height = 0;
+    int width = 0;
     int[][] mazeMatrix = {{}};
 
     public MazeGraph(int height, int width) 
     {
+        this.height = height;
+        this.width = width;
         try (Scanner inputMatrix = new Scanner(System.in)) 
         {
             for (int i = 0; i < height; i++) 
             {
                 for (int j = 0; j < width; j++)
-                {
                     mazeMatrix[i][j] = inputMatrix.nextInt();
-                }
             }
+        }
+    }
+
+    public void rightWallTrace()
+    {
+        Current current = new Current();
+        int[] finish = {this.height - 2, this.width - 1};
+        int right = mazeMatrix[current.pose[0] + current.right[0]][current.pose[1] + current.right[1]];
+        int front = 0; // implement later
+
+        while (current.pose != finish)
+        {
+            while (right == 1 && front != 1)
+            {
+                current.advance();
+                right = mazeMatrix[current.pose[0] + current.right[0]][current.pose[1] + current.right[1]];
+                Vertex v = new Vertex(current.pose[0], current.pose[1]);
+                this.vertices.add(v);
+                try 
+                {
+                    Vertex u = vertices.get(vertices.size() - 2);
+                    Edge e = new Edge(v, u);
+                    edges.add(e);
+                }
+                catch (Exception e) 
+                {}
+
+            }
+            current.turnRight();
+            right = mazeMatrix[current.pose[0] + current.right[0]][current.pose[1] + current.right[1]];
         }
     }
 

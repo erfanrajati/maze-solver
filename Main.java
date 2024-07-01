1ccdb17dd984fdd1716d21e0ae91e4a0a0b9c272
Erfan Rajati Haghi <erfan.rajati@gmail.com>
2024-07-01 23:05:56 +0330

    Matrix to graph feature added and works perfectly

--- Main.java
+++ Main.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class Main 
 {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         try (Scanner userIn = new Scanner(System.in)) {
             int mazeHeight = userIn.nextInt();
             int mazeWidth = userIn.nextInt();
             MazeGraph maze = new MazeGraph(mazeHeight, mazeWidth);
-            maze.rightWallTrace(); // now the matrix should be converted into a graph.
+            maze.buildGraphBFS(); // now the matrix should be converted into a graph.
             maze.getGraphData();
         }
     }
--- MazeGraph.java
+++ MazeGraph.java
@@ -1,188 +1,136 @@
 import java.util.*;
 
-enum Direction
-{
-    LEFT,
-    DOWN,
-    RIGHT,
-    UP
-}
+public class MazeGraph {
 
+    private class Vertex {
+        int i, j;
 
-public class MazeGraph 
-{
-    private class Vertex
-    {
-        int i = 0;
-        int j = 0;
-        public Vertex(int i, int j)
-        {
+        public Vertex(int i, int j) {
             this.i = i;
             this.j = j;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Vertex vertex = (Vertex) obj;
+            return i == vertex.i && j == vertex.j;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(i, j);
         }
 
         public String getVertex() {
             return i + " " + j;
         }
-        
     }
 
-    private class Edge 
-    {
-        Vertex[] edge;
-    
-        public Edge(Vertex v, Vertex u) 
-        {
-            edge = new Vertex[2];
-            this.edge[0] = v;
-            this.edge[1] = u;
+    private class Edge {
+        Vertex v, u;
+
+        public Edge(Vertex v, Vertex u) {
+            this.v = v;
+            this.u = u;
         }
 
-        public String getEdge()
-        {
-            return "(" + edge[0].i + ", " + edge[0].j + "), (" + edge[1].i + ", " + edge[1].j + ")";
-        }
-    }
-    
-    private class Current
-    {
-        int[] pose = {1, 0};
-        Direction face;
-        int[] right;
-
-        public Current()
-        {
-            this.face = Direction.RIGHT;
-            this.right[0] = 1;
-            this.right[1] = 0;
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Edge edge = (Edge) obj;
+            return (Objects.equals(v, edge.v) && Objects.equals(u, edge.u)) ||
+                    (Objects.equals(v, edge.u) && Objects.equals(u, edge.v));
         }
 
-        public void advance() 
-        {
-            if (this.face == Direction.RIGHT) 
-                this.pose[0] += 1;
-            if (this.face == Direction.DOWN) 
-                this.pose[1] += 1;
-            if (this.face == Direction.LEFT) 
-                this.pose[0] -= 1;
-            if (this.face == Direction.UP) 
-                this.pose[1] -= 1;
+        @Override
+        public int hashCode() {
+            return Objects.hash(v, u);
         }
 
-        public void turnRight()
-        {
-            if (this.face == Direction.RIGHT) 
-            {
-                this.face = Direction.DOWN;
-                this.right[0] = 0;
-                this.right[1] = -1;
-            }
-            if (this.face == Direction.DOWN) 
-            {
-                this.face = Direction.LEFT;
-                this.right[0] = -1;
-                this.right[1] = 0;
-            }
-            if (this.face == Direction.LEFT) 
-            {
-                this.face = Direction.UP;
-                this.right[0] = 0;
-                this.right[1] = 1;
-            }
-            if (this.face == Direction.UP) 
-            {
-                this.face = Direction.RIGHT;
-                this.right[0] = 1;
-                this.right[1] = 0;
-            }
+        public String getEdge() {
+            return "(" + v.i + ", " + v.j + "), (" + u.i + ", " + u.j + ")";
         }
     }
 
-    ArrayList<Vertex> vertices = new ArrayList<>();
-    ArrayList<Edge> edges = new ArrayList<>();
+    private final Set<Vertex> vertices = new HashSet<>();
+    private final Set<Edge> edges = new HashSet<>();
+    private final int height, width;
+    private final int[][] mazeMatrix;
 
-    int height = 0;
-    int width = 0;
-    int[][] mazeMatrix;
-
-    public MazeGraph(int height, int width) 
-    {
-        int temp;
+    public MazeGraph(int height, int width) {
         this.height = height;
         this.width = width;
+        this.mazeMatrix = new int[height][width];
 
-        mazeMatrix = new int[height][width];
-        
-        for (int[] mazeMatrix1 : mazeMatrix)
-            Arrays.fill(mazeMatrix1, -1);
-
-        try (Scanner inputMatrix = new Scanner(System.in)) 
-        {
-            for (int i = 0; i < height; i++) 
-            {
-                for (int j = 0; j < width; j++) 
-                {
-                    temp = inputMatrix.nextInt();
-                    // System.out.print(temp);
-                    mazeMatrix[i][j] = temp;
+        try (Scanner inputMatrix = new Scanner(System.in)) {
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (inputMatrix.hasNextInt()) {
+                        mazeMatrix[i][j] = inputMatrix.nextInt();
+                    } else {
+                        System.out.println("Error: Unexpected input at (" + i + ", " + j + ")");
+                        return;
+                    }                
                 }
             }
         }
     }
 
-    public void rightWallTrace()
-    {
-        Current current = new Current();
+    public void buildGraphBFS() {
+        boolean[][] visited = new boolean[height][width];
+        Queue<Vertex> queue = new LinkedList<>();
         int[] start = {1, 0};
-        int right = mazeMatrix[current.pose[0] + current.right[0]][current.pose[1] + current.right[1]];
 
-        int[][] mazeMatrixMod = mazeMatrix;
-        mazeMatrixMod[height - 2][width - 1] = 1;
+        if (mazeMatrix[start[0]][start[1]] == 1) {
+            System.out.println("Invalid start position.");
+            return;
+        }
 
-        while (current.pose != start)
-        {
-            while (right == 1)
-            {
-                current.advance();
-                right = mazeMatrixMod[current.pose[0] + current.right[0]][current.pose[1] + current.right[1]];
-                Vertex v = new Vertex(current.pose[0], current.pose[1]);
-                this.vertices.add(v); // Add vertex
-                try 
-                {
-                    Vertex u = vertices.get(vertices.size() - 2);
-                    Edge e = new Edge(v, u);
-                    edges.add(e); // Add edge
+        Vertex startVertex = new Vertex(start[0], start[1]);
+        queue.add(startVertex);
+        visited[start[0]][start[1]] = true;
+        vertices.add(startVertex);
+
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        while (!queue.isEmpty()) {
+            Vertex current = queue.poll();
+
+            for (int[] direction : directions) {
+                int ni = current.i + direction[0];
+                int nj = current.j + direction[1];
+
+                if (isInBounds(ni, nj) && mazeMatrix[ni][nj] == 0 && !visited[ni][nj]) {
+                    Vertex neighbor = new Vertex(ni, nj);
+                    queue.add(neighbor);
+                    visited[ni][nj] = true;
+                    vertices.add(neighbor);
+
+                    Edge edge = new Edge(current, neighbor);
+                    edges.add(edge);
                 }
-                catch (Exception e) 
-                {}
-
             }
-            current.turnRight();
-            right = mazeMatrixMod[current.pose[0] + current.right[0]][current.pose[1] + current.right[1]];
         }
     }
 
-    public void getGraphData()
-    {
+    private boolean isInBounds(int i, int j) {
+        return i >= 0 && i < height && j >= 0 && j < width;
+    }
+
+    public void getGraphData() {
         System.out.println("Vertices: ");
-        for (Vertex v : vertices)
+        for (Vertex v : vertices) {
             System.out.println(v.getVertex());
+        }
 
         System.out.println();
 
         System.out.println("Edges: ");
-        for (Edge e : edges)
+        for (Edge e : edges) {
             System.out.println(e.getEdge());
+        }
     }
-
-    // public void addVertex(int v) 
-    // {
-    //     vertices.add(v);
-    // }
-
-    // public void addEdge(int v, int u) 
-    // {
-    //     int[] edge = {v, u};
-    //     edges.add(edge);
-    // }
 }
